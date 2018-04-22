a42bc55d9fa31ea758a258a6ac8d82ba6d6040d6
Travyy <38111987+Travyy@users.noreply.github.com>
2018-04-22 16:27:49 -0500

    added comments

--- numbers.py
+++ numbers.py
@@ -13,6 +13,7 @@ def multiply_num():
     numbers = str_list_to_num_list(numbers)
     total = 0
     for num in numbers:
+        # make sure not multiplying by 0
         if total == 0:
             total += num
         else:
@@ -20,13 +21,16 @@ def multiply_num():
     print(total)
     print()
 
-
+    
+# im sure there is a way to not hardcode the numbers[0] & numbers[1] but really not sure it matters since
+# those are the only ones used in here anyway.
 def divide_num():
     numbers = []
     while len(numbers) <= 1 or len(numbers) > 2:
         numbers = input("Enter ONLY two numbers to divide separated by spaces(ex: dividend divisor): ")
         numbers = str_list_to_num_list(numbers)
         if len(numbers) == 2:
+            # make sure no division by 0
             if numbers[0] == 0 or numbers[1] == 0:
                 print("no division by 0, nice try. \n")
                 numbers = []
@@ -40,7 +44,8 @@ def divide_num():
         else:
             print("You entered too few numbers, try again \n")
 
-
+# im sure there is a way to not hardcode the numbers[0] & numbers[1] but really not sure it matters since
+# those are the only ones used in here anyway.
 def subtract_num():
     numbers = []
     while len(numbers) <= 1 or len(numbers) > 2:
@@ -76,7 +81,7 @@ def handle_user_choice(user_choice):
     elif user_choice == "exit":
         return "EXIT"
     else:
-        print("You have entered an invalid argument: Try again. \n")
+        print("You have entered an invalid argument: Try again. \n") # error if user types in incorrect string
 
 
 def get_user_input():
@@ -86,6 +91,8 @@ def get_user_input():
         if user_choice == '':
             user_choice = input("Choose a function you want to run; add, subtract, multiply, divide. If you want to " 
                                 "exit the program, type EXIT: ")
+            
+            # make sure input is only letters
             if not user_choice.isalpha():
                 print("You may only enter letters, no numbers allowed. Try Again. \n")
                 user_choice = ''
